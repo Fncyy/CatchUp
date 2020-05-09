6c6891f262017438abd999ec379419493b7f168e
Fncyy <penzes.martin7@gmail.com>
2020-05-09 13:50:26 +0200

    Two more tests

--- mobile/src/test/java/com/example/catchup/MainActivityTest.kt
+++ mobile/src/test/java/com/example/catchup/MainActivityTest.kt
@@ -1,7 +1,9 @@
 package com.example.catchup
 
+import android.content.Context
 import android.os.Build
 import android.view.View
+import android.view.animation.Animation
 import android.widget.ImageButton
 import androidx.recyclerview.widget.RecyclerView
 import org.junit.Assert.assertEquals
@@ -18,17 +20,11 @@ import org.robolectric.annotation.Config
 @Config(sdk = [Build.VERSION_CODES.P])
 class MainActivityTest {
 
-    private var activity: MainActivity? = null
-
-    @Before
-    fun init() {
-        activity = activity ?: Robolectric.buildActivity(MainActivity::class.java).create().resume().get()
-    }
-
     @Test
-    fun preferences_page_visibility() {
-        val preferencesButton: ImageButton = activity!!.findViewById(R.id.toolbar_main_preferences)
-        val recyclerView: RecyclerView = activity!!.findViewById(R.id.rvMain)
+    fun preferences_page_visibility_test() {
+        val activity = Robolectric.buildActivity(MainActivity::class.java).create().resume().get()
+        val preferencesButton: ImageButton = activity.findViewById(R.id.toolbar_main_preferences)
+        val recyclerView: RecyclerView = activity.findViewById(R.id.rvMain)
 
         assert(preferencesButton.isClickable)
         assertEquals(View.VISIBLE, recyclerView.visibility)
@@ -42,5 +38,39 @@ class MainActivityTest {
 
         assert(preferencesButton.isClickable)
         assertEquals(View.VISIBLE, recyclerView.visibility)
+    }
+
+    @Test
+    fun first_start_test() {
+        val activity = Robolectric.buildActivity(MainActivity::class.java)
+        var startedBefore = activity.get().getPreferences(Context.MODE_PRIVATE).getBoolean("KEY_STARTED", false)
+        assertFalse(startedBefore)
+        activity.create()
+        startedBefore = activity.get().getPreferences(Context.MODE_PRIVATE).getBoolean("KEY_STARTED", true)
+        assert(startedBefore)
+    }
+
+    @Test
+    fun reload_test() {
+        val activity = Robolectric.buildActivity(MainActivity::class.java).create().resume().get()
+        val refreshButton: ImageButton = activity!!.findViewById(R.id.toolbar_main_refresh)
+
+        refreshButton.performClick()
+        refreshButton.animation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationRepeat(animation: Animation?) {
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                assertEquals(true, animation?.hasEnded())
+            }
+
+            override fun onAnimationStart(animation: Animation?) {
+                assertEquals(1000, animation?.duration)
+                assertEquals(0, animation?.repeatCount)
+                assertEquals(true, animation?.fillAfter)
+                assertEquals(true, animation?.hasStarted())
+            }
+        })
+        refreshButton.performClick()
     }
 }
